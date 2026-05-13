540c88bdb9a24e3e83a2e556c40b7553e5fab564
Amandamandaanda <amandatanaskovic@gmail.com>
2026-05-13 10:32:49 +0200

    Initial Commit

---
+++ Snusless/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Snusless
+//
+//  Created by Amanda Tana on 2026-05-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Snusless/SnuslessApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnuslessApp.swift
+//  Snusless
+//
+//  Created by Amanda Tana on 2026-05-13.
+//
+
+import SwiftUI
+
+@main
+struct SnuslessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
