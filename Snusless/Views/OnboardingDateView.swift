75327fadbc372f92e8042ecc8686dff1f83df1c6
jonathan strid <pjstrid@outlook.com>
2026-05-19 12:27:07 +0100

    created basic layout for date picker in onboarding

--- Snusless/Views/OnboardingDateView.swift
+++ Snusless/Views/OnboardingDateView.swift
@@ -1,13 +1,13 @@
 //
-//  NameView.swift
+//  OnboardingDateView.swift
 //  Snusless
 //
-//  Created by Jonathan Strid on 2026-05-18.
+//  Created by Jonathan Strid on 2026-05-19.
 //
 
 import SwiftUI
 
-struct OnboardingNameView: View {
+struct OnboardingDateView: View {
     
     @Bindable var viewModel: OnboardingViewModel
 
@@ -15,31 +15,31 @@ struct OnboardingNameView: View {
         VStack(alignment: .center, spacing: 16) {
             Spacer()
 
-            Text("Välkommen!")
-                .font(.title)
-                .foregroundStyle(.white)
-                .bold()
-                .padding()
-
-            Text("Vänligen skriv ditt namn för att börja")
+            Text("Vilket datum slutade du snusa?")
                 .font(.title3)
                 .foregroundStyle(.white)
                 .bold()
                 .padding()
 
-            TextField("Ditt namn", text: $viewModel.name)
-                .bold()
-                .padding()
-                .foregroundStyle(.white)
+            DatePicker("Startdatum", selection: $viewModel.startDate, in: ...Date(), displayedComponents: .date)
+                .padding(.horizontal)
+                .environment(\.colorScheme, .dark)
+                .environment(\.locale, Locale(identifier: "sv_SE"))
+                .datePickerStyle(GraphicalDatePickerStyle())
                 .tint(.white)
                 .background {
                     Color(.gray.opacity(0.5))
                 }
                 .cornerRadius(20)
-
+            
+            Text("Valt datum: \(viewModel.startDate.formatted(.dateTime.day().month(.wide).year().locale(Locale(identifier: "sv_SE"))))")
+                .foregroundStyle(.white)
+                .bold()
+            
             Spacer()
 
             HStack {
+                
                 Spacer()
                 Button {
                     Task {
@@ -51,12 +51,11 @@ struct OnboardingNameView: View {
                             .font(.title2)
                             .bold()
                             .padding(.horizontal)
-                            .foregroundStyle(viewModel.name.isEmpty ? .gray : .white)
+                            .foregroundStyle(.white)
                     }
                 }
                 .buttonStyle(.bordered)
                 .font(Font.title3.bold())
-                .disabled(viewModel.name.isEmpty)
             }
         }
         .padding()
@@ -65,5 +64,5 @@ struct OnboardingNameView: View {
 }
 
 #Preview {
-    OnboardingNameView(viewModel: OnboardingViewModel())
+    OnboardingDateView(viewModel: OnboardingViewModel())
 }
