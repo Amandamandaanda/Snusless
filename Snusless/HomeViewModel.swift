af56fe76777ea09894a534bf83fdea000d7fb405
Amandamandaanda <amandatanaskovic@gmail.com>
2026-05-19 18:59:31 +0200

    Added toggle function for daily checks in viewmodel.

--- Snusless/HomeViewModel.swift
+++ Snusless/HomeViewModel.swift
@@ -11,4 +11,26 @@ import Foundation
 @Observable
 class HomeViewModel{
     
+    var errorMessage: String?
+    
+    func checkToday(streak: Streak, context: ModelContext) {
+        
+        errorMessage = nil
+        
+        let today = Calendar.current.startOfDay(for: Date())
+        
+        if let index = streak.completedDays.firstIndex(where: {
+            Calendar.current.isDateInToday($0)
+        }) {
+            streak.completedDays.remove(at: index)
+        } else {
+            streak.completedDays.append(today)
+        }
+        
+        do {
+            try context.save()
+        } catch {
+            errorMessage = "Kunde inte spara"
+        }
+    }
 }
