2fad81fae4724618af52ce8015c4053b3511e419
Amandamandaanda <amandatanaskovic@gmail.com>
2026-05-25 10:49:39 +0200

    Added files for settingsview and viewmodel.

---
+++ Snusless/ViewModels/SettingsViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsViewModel.swift
+//  Snusless
+//
+//  Created by Amanda Tana on 2026-05-25.
+//
+
+import SwiftData
+import Foundation
+
+@Observable
+class SettingsViewModel {
+    
+}
---
+++ Snusless/Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  Snusless
+//
+//  Created by Amanda Tana on 2026-05-25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct SettingsView: View {
+    var body: some View {
+      
+    }
+}
+
+
+#Preview {
+    SettingsView()
+}
