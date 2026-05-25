50a1af3adbbe6b2092aa5f5414d68c9229209d19
Amandamandaanda <amandatanaskovic@gmail.com>
2026-05-25 11:08:07 +0200

    Fixed basic ui desing for settings.

--- Snusless/Views/SettingsView.swift
+++ Snusless/Views/SettingsView.swift
@@ -9,10 +9,36 @@ import SwiftUI
 import SwiftData
 
 struct SettingsView: View {
+    @Query private var users: [User]
+    
+    @Environment(\.modelContext) private var modelContext: ModelContext
     
     @State private var viewModel = SettingsViewModel()
     
     var body: some View {
+        NavigationStack {
+            VStack(spacing: 24) {
+                Button {
+                    
+                } label: {
+                    HStack {
+                        Text("Rensa data")
+                            .foregroundStyle(.red)
+                        
+                        Spacer()
+                        
+                        Image(systemName: "trash")
+                            .foregroundStyle(.red)
+                            .background(Color.red.opacity(0.1))
+                            .clipShape(RoundedRectangle(cornerRadius: 16))
+                    }
+                    Spacer()
+                }
+                .padding()
+                .navigationTitle("Inställningar")
+            
+            }
+        }
       
     }
 }
