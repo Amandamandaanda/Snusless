abec6bc46a319ab4617596d8fb24d7112c38dbd0
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-05-26 09:54:52 +0200

    Created intial profileview and navigationlink to be able to create tabnavigation and to navigate to settingsView

---
+++ Snusless/Views/ProfileView.swift
@@ -0,0 +1,34 @@
+//
+//  ProfileView.swift
+//  Snusless
+//
+//  Created by Aurelie Vaudan APP25 on 2026-05-26.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                NavigationLink {
+                    // SettingsView()
+                } label: {
+                    Image(systemName: "gear")
+                }
+         
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        ProfileView()
+
+    }
+    
+}
