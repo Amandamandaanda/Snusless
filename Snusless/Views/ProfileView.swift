a81623cd7825b70671b43324ca98a28ded878552
Pinar <pinarbildirici87@gmail.com>
2026-05-27 09:08:43 +0200

    Implement fields for user data and match application design

--- Snusless/Views/ProfileView.swift
+++ Snusless/Views/ProfileView.swift
@@ -17,21 +17,69 @@ struct ProfileView: View {
         @State private var editDosor: Int = 0
         @State private var editPrice: Double = 0.0
     var body: some View {
-        VStack {
-            
+
+            VStack(spacing: 20) {
+                Text("Ändra profil")
+                    .font(.custom("Roboto-Bold", size: 24))
+                    .foregroundColor(.white)
+                    .padding(.top, 10)
+                        
+    ScrollView {
+            VStack(spacing: 20) {
+                               
+            VStack(alignment: .leading, spacing: 8) {
+            Text("Namn")
+           .font(.custom("Roboto-Bold", size: 16))
+           .foregroundColor(.white)
+                                    
+    TextField("", text: $editName)
+    .font(.custom("Roboto-Light", size: 18))
+    .padding()
+    .background(Color.white)
+    .foregroundColor(.black)
+    .cornerRadius(10)
         }
-        .toolbar {
-            ToolbarItem(placement: .topBarTrailing) {
-                NavigationLink {
-                    SettingsView()
-                } label: {
-                    Image(systemName: "gear")
-                }
-         
+    .padding(.horizontal, 20)
+                                
+                               
+   VStack(alignment: .leading, spacing: 8) {
+       Text("Antal snusdosor per dag")
+        .font(.custom("Roboto-Bold", size: 16))
+        .foregroundColor(.white)
+                                    
+    TextField("", value: $editDosor, format: .number)
+    .keyboardType(.numberPad)
+    .font(.custom("Roboto-Light", size: 18))
+    .padding()
+    .background(Color.white)
+    .foregroundColor(.black)
+    .cornerRadius(10)
+        }
+    .padding(.horizontal, 20)
+                                
+                                
+        VStack(alignment: .leading, spacing: 8)  {                      Text("Pris per snusdosa (kr)")
+       .font(.custom("Roboto-Bold", size: 16))
+       .foregroundColor(.white)
+                                    
+       TextField("", value: $editPrice, format: .number)
+       .keyboardType(.decimalPad)
+       .font(.custom("Roboto-Light", size: 18))
+       .padding()
+    .background(Color.white)
+    .foregroundColor(.black)
+    .cornerRadius(10)
+            }
+    .padding(.horizontal, 20)
+                            }
+    .padding(.vertical, 10)
+                        }
+                        
+    Spacer()
+                   }
             }
         }
-    }
-}
+  
 
 #Preview {
     NavigationStack {
