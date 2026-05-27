c8c1622792c53255996da8cbae0977d904ddc2a5
Pinar <pinarbildirici87@gmail.com>
2026-05-27 08:37:34 +0200

    Added updateUser function to OnboardingViewModel for profile uppdates

--- Snusless/ViewModels/OnboardingViewModel.swift
+++ Snusless/ViewModels/OnboardingViewModel.swift
@@ -67,24 +67,42 @@ class OnboardingViewModel {
     }
     
     func validation() -> Bool {
-        guard !name.trimmingCharacters(in: .whitespaces).isEmpty else {
-            errorMessage = "Vänligen fyll i ditt namn"
-            return false
+            guard !name.trimmingCharacters(in: .whitespaces).isEmpty else {
+                errorMessage = "Vänligen fyll i ditt namn"
+                return false
+            }
+            
+            guard (numberOfDosor ?? 0) > 0 else {
+                errorMessage = "Antalet dosor får inte vara noll"
+                return false
+            }
+            
+            guard (pricePerDosa ?? 0) > 0.0  else {
+                errorMessage = "Vänligen fyll i priset för din snusdosa. Priset får inte vara noll"
+                return false
+            }
+            
+            guard portionsPerDosa > 0 else {
+                errorMessage = "Vänligen fyll i hur många portioner din dosa innehåller."
+                return false
+            }
+            return true
         }
         
-        guard (numberOfDosor ?? 0) > 0 else {
-            errorMessage = "Antalet dosor får inte vara noll"
-            return false
+       
+        func updateUser(context: ModelContext, updatedName: String, updatedDosor: Int, updatedPrice: Double) {
+            do {
+                let descriptor = FetchDescriptor<User>()
+                if let existingUser = try context.fetch(descriptor).first {
+                    existingUser.name = updatedName.trimmingCharacters(in: .whitespaces)
+                    existingUser.numberOfDosor = updatedDosor
+                    existingUser.pricePerDosa = updatedPrice
+                    
+                    try context.save()
+                    print("Användardata har uppdaterats i SwiftData")
+                }
+            } catch {
+                print("Misslyckades med att uppdatera användaren: \(error.localizedDescription)")
+            }
         }
-        
-        guard (pricePerDosa ?? 0) > 0.0  else {
-            errorMessage = "Vänligen fyll i priset för din snusdosa. Priset får inte vara noll"
-            return false
-        }
-        guard portionsPerDosa > 0 else {
-            errorMessage = "Vänligen fyll i hur många portioner din dosa innehåller."
-            return false
-        }
-        return true
     }
-}
