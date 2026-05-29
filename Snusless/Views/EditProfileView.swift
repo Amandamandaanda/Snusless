637a7d48fc1993684666084eca00c2a38ade743a
jonathan strid <pjstrid@outlook.com>
2026-05-29 10:24:58 +0100

    added keyboardtype .numberPad to text fields in EditProfileView

--- Snusless/Views/EditProfileView.swift
+++ Snusless/Views/EditProfileView.swift
@@ -53,21 +53,25 @@ struct EditProfileView: View {
                 .font(.custom("Roboto-Medium", size: 16))
 
             TextField("\(editProfileViewModel.editDosor) dosor", value: $editVM.editDosor, format: .number)
+                .keyboardType(.numberPad)
                 .modifier(EditProfileTextFieldModifier())
 
             Text("Portioner per snusdosa")
                 .font(.custom("Roboto-Medium", size: 16))
 
             TextField("\(editProfileViewModel.editPortioner) st", value: $editVM.editPortioner, format: .number)
+                .keyboardType(.numberPad)
                 .modifier(EditProfileTextFieldModifier(keyboardType: .numberPad))
 
             Text("Pris per snusdosa (kr)")
                 .font(.custom("Roboto-Medium", size: 16))
 
             TextField("\(String(format: "%.2f", editProfileViewModel.editPrice)) kr", value: $editVM.editPrice, format: .number)
+                .keyboardType(.numberPad)
                 .modifier(EditProfileTextFieldModifier())
 
             Text("Sparmål (kr)")
+                .keyboardType(.numberPad)
                 .font(.custom("Roboto-Medium", size: 16))
 
             TextField("\(editProfileViewModel.editSavingsGoal) kr", value: $editVM.editSavingsGoal, format: .number)
