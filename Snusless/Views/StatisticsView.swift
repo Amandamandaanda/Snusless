98d86105e51d7f065b544f9ded2d938ba45ec186
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-06-03 14:19:13 +0200

    Initial design for statisticsView

---
+++ Snusless/Views/StatisticsView.swift
@@ -0,0 +1,57 @@
+//
+//  StatisticsView.swift
+//  Snusless
+//
+//  Created by Aurelie Vaudan APP25 on 2026-06-03.
+//
+
+import SwiftUI
+import SwiftData
+
+
+struct StatisticsView: View {
+  
+
+    @State private var viewModel = HomeViewModel()
+    @Query private var users: [User]
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            if let user = users.first {
+                StatisticsCard(
+                    title: String(localized: "Du har klarat dig utan:"),
+                    description: String(format: String(localized:  "%.0f portioner snus"), user.calculatePortions())
+                )
+                
+                
+                
+                StatisticsCard(title: String(localized: "Max rekord snus utan att ta snus"), description: user.totalSnusNotTaken == 0 ? String(format: "%.0f portioner snus", user.calculatePortions()) : String(format: String(localized: "%.0f portioner snus"), user.totalSnusNotTaken))
+                
+                StatisticsCard(title: String(localized: "Du har sparat:"), description: "\(user.totalSaved) kr")
+                
+                if user.snusTakenCount > 0 {
+                    StatisticsCard(title: String(localized: "Du har tagit:"), description: "\(user.snusTakenCount) \(String(localized: "portioner snus"))"
+                                 , foregroundColorTitle: .errorRed)
+                }
+                
+                Spacer()
+            }
+            
+        }
+        .frame(maxHeight: .infinity)
+        .toolbar {
+            ToolbarItem(placement: .title) {
+                Text("Statistik")
+                    .font(.custom("Roboto-Bold", size: 20))
+                    .foregroundColor(.black)
+            }
+
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        StatisticsView()
+    }
+}
